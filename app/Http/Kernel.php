d8644dd54c9a5532ef71335204244138b004640c
mdnahidislam <mdnahidislam30@gmail.com>
2022-01-05 21:53:22 +0600

    Admin middleware added for admin login and local user login

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        // Custom Admin middleware class below
+        'isAdmin' => \App\Http\Middleware\AdminMiddleware::class,
     ];
 }
